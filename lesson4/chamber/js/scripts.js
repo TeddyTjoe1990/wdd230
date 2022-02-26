3c9477bd4d285210547766549277736a1b9c74da
TeddyTjoe1990 <her21059@byui.edu>
2022-02-26 13:42:42 +0700

    update JS and html file

--- lesson4/chamber/js/scripts.js
+++ lesson4/chamber/js/scripts.js
@@ -122,3 +122,12 @@ if ("IntersectionObserver" in window) {
         loadImages(img);
     });
 }
+
+// JOIN PAGE CURRENT DATE AND TIME
+const joinDate = document.querySelector("#joinDate");
+joinDate.textContent = currentDate;
+
+const time = new Date().toLocaleTimeString();
+
+const joinTime = document.querySelector("#joinTime");
+joinTime.textContent = time;
