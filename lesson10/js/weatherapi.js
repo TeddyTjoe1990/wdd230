b6d46ec85ed60d3be39a62f6340f1141a93db17c
TeddyTjoe1990 <her21059@byui.edu>
2022-03-14 19:12:47 +0700

    lesson 10 was made

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -2,6 +2,18 @@ const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&unit
 
 fetch(apiURL)
     .then((response) => response.json())
-    .then((jsObject) => {
-        console.log(jsObject);
+    .then((x) => {
+        //console.log(x);
+
+        const currentTemp = document.querySelector('#current-temp');
+        const weathericon = document.querySelector("#weathericon");
+
+        currentTemp.textContent = x.main.temp.toFixed(0);
+        let imgsrc = `https://openweathermap.org/img/w/${x.weather[0].icon}.png`;
+        let imgalt = x.weather[0].description;
+        //imgalt = imgalt.split(' ').map(capitalize).join(' ');
+
+        currentTemp.textContent = x.main.temp.toFixed(0);
+        weathericon.setAttribute('src', imgsrc);
+        weathericon.setAttribute('alt', imgalt);
     });
