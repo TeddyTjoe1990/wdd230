140792871ce7f36114e271a7c9e9f3f190e7f139
TeddyTjoe1990 <her21059@byui.edu>
2022-04-09 16:09:06 +0700

    made some updates

---
+++ temple/js/temples.js
@@ -0,0 +1,119 @@
+//--- TEMPLES --- //
+const requestURL = "data/temples.json";
+
+fetch(requestURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(templesObject) {
+        // console.table(templesObject); // temporary checking for valid response and data parsing
+        const temples = templesObject["temples"];
+        temples.forEach(displayTemples);
+    });
+
+function displayTemples(temple) {
+    // Create elements to add to the document
+    let card = document.createElement("div");
+    let title = document.createElement("div");
+    let h3 = document.createElement("h3");
+    let para1 = document.createElement("p");
+    let para2 = document.createElement("p");
+    let para3 = document.createElement("p");
+    let para4 = document.createElement("p");
+    let para5 = document.createElement("p");
+    let para6 = document.createElement("p");
+    let para7 = document.createElement("p");
+    let para8 = document.createElement("p");
+    let img = document.createElement("img");
+    let icon = document.createElement("i");
+    let body = document.querySelector("body");
+
+    let dir = document.querySelector("#directory");
+    const vw = Math.max(
+        document.documentElement.clientWidth || 0,
+        window.innerWidth || 0
+    );
+
+    let t_history = temple.history;
+    let t_closure = temple.closure;
+
+    // Extract first name of temple
+    let arr_temple_name = temple.name.split(" ");
+    let temple_id = arr_temple_name[0];
+    // console.log(temple_id);
+
+    // Change the textContent property of the elements to contain company info
+    h3.textContent = `${temple.name}`;
+    h3.setAttribute("id", `${temple_id}`);
+    icon.setAttribute("class", "fa-solid fa-thumbs-up");
+    icon.setAttribute("id", `${temple_id}`);
+    para1.innerHTML = `<b>Address:</b> ${temple.address}`;
+    para2.innerHTML = `<b>Phone:</b> ${temple.phone}`;
+    para3.innerHTML = `Send email via temple <a href="${temple.email}" target="_blank">template</a> `;
+    para4.innerHTML = `<b>Services:</b> ${temple.services}`;
+    para5.innerHTML = `<b>Ordinance and Session:</b> ${temple.ordinance}`;
+    img.setAttribute("src", `${temple.image}`);
+    img.setAttribute("alt", `Image - ${temple.name}`);
+    img.setAttribute("loading", "lazy");
+    img.setAttribute("width", "420");
+    img.setAttribute("height", "240");
+
+    // Add/append the section(card)
+    card.appendChild(img);
+    img.style.width = "100%";
+    card.appendChild(title);
+    title.classList.add("temple_title");
+    title.appendChild(h3);
+    title.appendChild(icon);
+    card.classList.add("temple_item");
+
+    h3.style.fontWeight = "bold";
+    card.appendChild(para1);
+    card.appendChild(para2);
+    card.appendChild(para3);
+    card.appendChild(para4);
+    card.appendChild(para5);
+    card.appendChild(para6);
+    card.appendChild(para7);
+
+    // like icon - local storage (single item):: WORKING!
+    function likedTempleIcon() {
+        icon.classList.toggle("liked");
+        if (icon.classList.contains("liked")) {
+            para8.textContent = `Liked!`;
+            title.appendChild(para8);
+            localStorage.setItem("likedTemple", temple_id);
+        }
+    }
+
+    icon.onclick = likedTempleIcon;
+
+    const lt = localStorage.getItem("likedTemple");
+
+    if (icon.id == lt) {
+        icon.classList.add("liked");
+        para8.textContent = "Liked";
+        title.appendChild(para8);
+    }
+
+    // temple history : add <ul> + <li> for each history item
+    let hx = "<ul>";
+    t_history.forEach(function(item) {
+        hx += "<li>" + item + "</li>";
+    });
+
+    hx += "</ul>";
+    para6.innerHTML = `<b>Temple History:</b> ${hx}`;
+
+    // temple closure : add <ul> + <li> for each temple closre item
+    let closure = "<ul>";
+    t_closure.forEach(function(item) {
+        closure += "<li>" + item + "</li>";
+    });
+
+    closure += "</ul>";
+    para7.innerHTML = `<b>Temple Closure:</b> ${closure}`;
+
+    // add card to div with .dir class
+    document.querySelector("div.dir").appendChild(card);
+}
---
+++ temple/js/weather.js
@@ -0,0 +1,163 @@
+//-- CURRENT WEATHER --//
+
+const apiURL =
+  "//api.openweathermap.org/data/2.5/onecall?lat=38.9807&lon=-77.1003&units=imperial&appid=6a8e47c47709fc9a90e95c0d8af1e420";
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((weatherInfo) => {
+    // console.log(weatherInfo);
+
+    let city = weatherInfo.current;
+    let city_weather = city.weather;
+    let city_humidity = city.humidity;
+
+    // console.log(`${city_weather[0].description}`);
+
+    // City Temperature
+    let temp = city.temp;
+    let wholeTemp = Math.round(temp);
+    document.querySelector("#temp").textContent = wholeTemp;
+
+    // Weather Image
+    const iconsrc = `//openweathermap.org/img/wn/${city_weather[0].icon}@2x.png`;
+
+    // Weather Forecast
+    const desc = city_weather[0].description;
+
+    // Capitalize First letter of words for the forecast
+    function capitalizeFirst(word) {
+      split_word = word.split(" ");
+
+      // Capitalize each first letter of weather forecast
+      for (let i = 0; i < split_word.length; i++) {
+        split_word[i] =
+          split_word[i][0].toUpperCase() + split_word[i].substr(1);
+      }
+
+      // Join and return split capitalize words
+      let joined_word = split_word.join(" ");
+      return joined_word;
+    }
+
+    let main_desc = capitalizeFirst(desc);
+
+    // Add weather image and forecast to html page
+    document.querySelector("#weathericon").setAttribute("src", iconsrc);
+    document.querySelector("#weathericon").setAttribute("alt", desc);
+    document.querySelector(".forecast").textContent = main_desc;
+
+    // Get humidity and add to html page
+    let humidity = city_humidity;
+    document.querySelector("#humidity").textContent = humidity;
+
+    // Weather Alert
+    const weather_alert = document.querySelector("#weather_alert");
+
+    // If City has weather alert
+    if (weatherInfo.alerts == true) {
+      // let city_alerts_desc = weatherInfo.alerts[0].description;
+
+      // console.log(city_alerts_desc);
+      alerts_desc = city_alerts.description.replaceAll("...", "</br>");
+      alerts_clean = alerts_desc.replaceAll("*", "<hr>");
+
+      // Add Weather Alert to HTML
+      weather_alert.innerHTML = `<div id="close_btn"><h3>${city_alerts.event}</h3><i class="fa fa-window-close" aria-hidden="true"></i></div><div class="mssg" >${alerts_clean}<p><i>Source: ${city_alerts.sender_name}</i></p></div>`;
+
+      // Close button to close alert
+      let close_btn = document.querySelector("#close_btn");
+      close_btn.addEventListener("click", closeText);
+
+      // Show Weather alert on top of page
+      weather_alert.classList.add("show");
+
+      // Function to close weather alert section
+      function closeText() {
+        weather_alert.classList.add("hide");
+      }
+    } else {
+      // if city has not weather alert, hide alert section
+      weather_alert.classList.add("hide");
+    }
+
+    // ----------- 3 DAY FORECAST ------------- //
+    let city_daily = weatherInfo.daily;
+    let city_d1 = city_daily[0].temp.day;
+    let city_d2 = city_daily[1].temp.day;
+    let city_d3 = city_daily[2].temp.day;
+    // console.log(city_d1)
+
+    //----- FORECAST 1 ---//
+
+    // FORECAST 1 Temperature
+    let d1_temp = Math.round(city_d1);
+    document.querySelector("#temp_1").textContent = d1_temp;
+
+    // Forecast Weather Image
+    const iconsrc_1 = `//openweathermap.org/img/wn/${city_daily[0].weather[0].icon}@2x.png`;
+
+    // Weather Forecast
+    const desc1 = city_daily[0].weather[0].description;
+
+    let forecast1_desc = capitalizeFirst(desc1);
+
+    // Add weather image and forecast to html page
+    document.querySelector("#weathericon_1").setAttribute("src", iconsrc_1);
+    document
+      .querySelector("#weathericon_1")
+      .setAttribute("alt", forecast1_desc);
+    document.querySelector(".forecast_1").textContent = forecast1_desc;
+
+    //----- FORECAST 2 ---//
+
+    // FORECAST 2 Temperature
+    let d2_temp = Math.round(city_d2);
+    document.querySelector("#temp_2").textContent = d2_temp;
+
+    // Forecast Weather Image
+    const iconsrc_2 = `//openweathermap.org/img/wn/${city_daily[1].weather[0].icon}@2x.png`;
+
+    // Weather Forecast
+    const desc2 = city_daily[1].weather[0].description;
+
+    let forecast2_desc = capitalizeFirst(desc2);
+
+    // Add weather image and forecast to html page
+    document.querySelector("#weathericon_2").setAttribute("src", iconsrc_2);
+    document
+      .querySelector("#weathericon_2")
+      .setAttribute("alt", forecast1_desc);
+    document.querySelector(".forecast_2").textContent = forecast2_desc;
+
+    //----- FORECAST 3 ---//
+
+    // FORECAST 3 Temperature
+    let d3_temp = Math.round(city_d3);
+    document.querySelector("#temp_3").textContent = d3_temp;
+
+    // Forecast Weather Image
+    const iconsrc_3 = `//openweathermap.org/img/wn/${city_daily[2].weather[0].icon}@2x.png`;
+
+    // Weather Forecast
+    const desc3 = city_daily[2].weather[0].description;
+
+    let forecast3_desc = capitalizeFirst(desc2);
+
+    // Add weather image and forecast to html page
+    document.querySelector("#weathericon_3").setAttribute("src", iconsrc_3);
+    document
+      .querySelector("#weathericon_3")
+      .setAttribute("alt", forecast1_desc);
+    document.querySelector(".forecast_3").textContent = forecast3_desc;
+  });
+
+  // Weather Date Display
+document.querySelector("#weather_date").textContent = `From ${currentDate}`;
+document.querySelector("#weather_date1").textContent = `${todaysDate} ${month}`;
+document.querySelector("#weather_date2").textContent = `${
+  todaysDate + 1
+} ${month}`;
+document.querySelector("#weather_date3").textContent = `${
+  todaysDate + 2
+} ${month}`;
